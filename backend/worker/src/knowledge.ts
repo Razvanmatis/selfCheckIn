7f737d6f0d865ab4f696741621431317d8fec4bc
Razvan Matis <razvan.matis@dv-ma.de>
2026-08-28 12:59:53 +0200

    introduced rules dialogs

---
+++ backend/worker/src/knowledge.ts
@@ -0,0 +1,30 @@
+export const knowledge = [
+    {
+        id: "washing-machine",
+        text: `
+      Die Waschmaschine befindet sich in der Küche.
+      Bitte ausschließlich Kurzwaschprogramme mit max. 30Min oder 1Stunde nutzen. 
+      Bitte auch sicherstellen dass nicht zu wenig Wäsche gewaschen wird. 
+      Ansonsten auch gerne auch auf mich zukommen und mich fragen ob ich auch etwas mitwaschen möchte.
+      Als Waschmittel stehen links neben der Maschine Ariel Pods bereit, welche gerne verwendet werden dürfen. Zusätzlich 
+      befindet sich hinten links an der Maschine auch Weichspüler, welcher ebenfalls gerne verwendet werden darf.
+    `,
+        language: "de",
+    },
+    {
+        id: "trash",
+        text: `
+      Es befinden sich alle notwendigen Müllcontainer draußen im Außenhof, der über die untere Tür des Hauses erreichbar ist.
+      Diese Container können auch von meinem Balkon aus gesehen werden.
+    `,
+        language: "de",
+    },
+    {
+        id: "wifi",
+        text: `
+      Der WLAN-Router befindet sich im Wohnzimmer.
+      Netzwerkname lautet "raz-wlan" und das Passwort lautet: "RazorsWlan123"
+    `,
+        language: "de",
+    },
+];
